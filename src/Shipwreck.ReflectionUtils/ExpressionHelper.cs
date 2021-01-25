3e90d4ccec654220ab04fdb649ed8b3aaa133df3
pgrho <shipwreck@live.jp>
2021-01-25 22:14:49 +0900

    Added ExpressionHelper

---
+++ src/Shipwreck.ReflectionUtils/ExpressionHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Shipwreck.ReflectionUtils
+{
+    public static class ExpressionHelper
+    {
+        public static string GetMemberPath<T, TProperty>(this Expression<Func<T, TProperty>> expression)
+        {
+            StringBuilder sb = null;
+            for (var e = expression.Body as MemberExpression; e != null; e = e.Expression as MemberExpression)
+            {
+                if (sb == null)
+                {
+                    sb = new StringBuilder(e.Member.Name);
+                }
+                else
+                {
+                    sb.Insert(0, e.Member.Name);
+                    sb.Insert(e.Member.Name.Length, '.');
+                }
+            }
+            return sb?.ToString();
+        }
+    }
+}
