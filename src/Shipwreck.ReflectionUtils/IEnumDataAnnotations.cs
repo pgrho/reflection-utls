11646a25bde39fa0fa08d18ff79c81bb644acce7
pgrho <shipwreck@live.jp>
2021-01-31 22:16:20 +0900

    Added EnumDataAnnotations

---
+++ src/Shipwreck.ReflectionUtils/IEnumDataAnnotations.cs
@@ -0,0 +1,15 @@
+﻿namespace Shipwreck.ReflectionUtils
+{
+    public interface IEnumDataAnnotations
+    {
+        string GetDisplayName(object value);
+
+        string GetShortName(object value);
+
+        string GetGroupName(object value);
+
+        string GetDescription(object value);
+
+        string GetPrompt(object value);
+    }
+}
